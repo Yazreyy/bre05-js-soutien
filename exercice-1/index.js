c1711602215ef155552d1b0d8c8c66b203d4d5d9
Marlonn Gillet <gillet.marlonn56@icloud.com>
2026-01-09 14:48:54 +0100

    exo 2

---
+++ exercice-1/index.js
@@ -0,0 +1,16 @@
+let arrays = [1,2,3,4,5]
+
+function displayArray(arrays){
+    arrays.forEach(element => console.log(element));
+}
+
+displayArray(arrays);
+
+const tableau2 = [10, 20, 30, 40, 50];
+const tableau3 = [5, 4, 3, 2, 1];
+const tableau4 = [100, 200, 300, 400, 500];
+
+displayArray(tableau2);
+displayArray(tableau3);
+displayArray(tableau4);
+
---
+++ exercice-2/index.js
@@ -0,0 +1,11 @@
+
+
+function displayarrayreverse(arrayreverse){
+for (let i = arrayreverse.length - 1 ; i >= 0 ; i--){
+    console.log(arrayreverse[i])
+}
+}
+displayarrayreverse([1,2,3,4,5])
+displayarrayreverse([10,20,30,40,50]);
+displayarrayreverse([5,4,3,2,1]);
+displayarrayreverse([100,200,300,400,500]);
